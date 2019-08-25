d871c330ce029a59746b0ba6bed563c2344c3d98
wonae789 <wonae789@naver.com>
2019-08-25 17:34:24 +0900

    test upload2

--- C_STUDY/C_STUDY/test.c
+++ C_STUDY/C_STUDY/test.c
@@ -1,12 +1,41 @@
 #include<stdio.h>
 #include<string.h> // strlen 사용
 
+
+typedef enum syllable
+{
+	mon = 1, thu = 2
+}syllable;
+
 int main(void)
 {
+	syllable to;
+	int i = mon;
+	printf("%d", to);
 
-	char a[20] = "aaa";
-
-	printf("%d\n", sizeof(a));
-	printf("%d\n ", strlen(a));
 	return 0;
-}  
+}
+
+
+
+//int main(void)
+//{
+//
+//	char a[20] = "aaa";
+//
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n ", strlen(a));
+//	return 0;
+//}   
+
+//int main(void)
+//{
+//
+//	char str[3];
+//	printf("정수 입력 : ");
+//	scanf("%s", str);
+//
+//	printf("%s", str);
+//
+//	return 0;
+//}
